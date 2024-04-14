314818d1cf064a260ffd6ec501883388c6f7506e
navneeth-mn <navneethnavu108@gmail.com>
2024-04-14 09:47:00 +0530

    ok

--- src/main/java/com/stackroute/lambdaexpression/AlphabetChecker.java
+++ src/main/java/com/stackroute/lambdaexpression/AlphabetChecker.java
@@ -1,10 +1,25 @@
 package com.stackroute.lambdaexpression;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlphabetChecker {
     //write logic to find whether given string contains only alphabets or not
     public String checkAlphabets(List<String> inputList) {
-        return null;
+        CheckAlpha answer;
+        answer = (input) -> {
+            for (String word:input
+            ) {
+                if(!word.trim().matches("[a-zA-Z]+"))
+                    return "Given list contains non alphabet strings";
+            }
+            return  "Given list contains only alphabet strings";
+        };
+
+        if (inputList.isEmpty())
+            return "Give proper input not empty list";
+
+        else
+            return answer.checkValidity(inputList);
     }
 }
---
+++ src/main/java/com/stackroute/lambdaexpression/CheckAlpha.java
@@ -0,0 +1,7 @@
+package com.stackroute.lambdaexpression;
+
+import java.util.List;
+
+public interface CheckAlpha {
+    public String checkValidity(List<String> inputList);
+}
